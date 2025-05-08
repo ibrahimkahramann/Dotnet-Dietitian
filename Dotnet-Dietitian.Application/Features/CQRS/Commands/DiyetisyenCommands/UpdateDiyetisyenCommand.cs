a3f5dbdcbc1e18cd084a7eaa232b7e6b1eadefa4
ibrahimkahramann <ibrahimk.bg06@gmail.com>
2025-05-08 14:29:20 +0300

    feat: add command classes for Diyetsiyen

---
+++ Dotnet-Dietitian.Application/Features/CQRS/Commands/DiyetisyenCommands/UpdateDiyetisyenCommand.cs
@@ -0,0 +1,20 @@
+using MediatR;
+
+namespace Dotnet_Dietitian.Application.Features.CQRS.Commands.DiyetisyenCommands
+{
+    public class UpdateDiyetisyenCommand : IRequest<Unit>
+    {
+        public Guid Id { get; set; }
+        public string TcKimlikNumarasi { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string? Uzmanlik { get; set; }
+        public string Email { get; set; }
+        public string? Telefon { get; set; }
+        public string? MezuniyetOkulu { get; set; }
+        public int? DeneyimYili { get; set; }
+        public string? Hakkinda { get; set; }
+        public string? ProfilResmiUrl { get; set; }
+        public string? Sehir { get; set; }
+    }
+}
